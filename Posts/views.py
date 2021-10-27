1a8d2e41d429873494409c98ecaf1241d1433c7b
Saaransh678 <saaransh678@gmail.com>
2021-10-27 10:52:50 +0530

    Like added

--- Posts/views.py
+++ Posts/views.py
@@ -7,6 +7,6 @@ def Home_View(request):
     return render(request, 'Pages/Home_Page.html', context={}, status=200)
 def post_list_view(request):
     qs=Posts.objects.all()
-    posts_list = [{"id":x.id,"content":x.content} for x in qs]
+    posts_list = [{"id":x.id,"content":x.content, "Likes":12} for x in qs]
     data = {"response":posts_list}
     return JsonResponse(data)
